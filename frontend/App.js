4be0259aabdb68f96940c278a8e4a6eb16cb25cc
LeeJaeHoon <dlwogns3413@naver.com>
2024-03-31 17:45:01 +0900

    [FE] fix: Join page 삭제

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import Home from "./page/Home/Home";
-import Join from "./page/Join/Join";
 import Header from "./components/Header/Header";
 import EmailInputPage from "./page/Join/EmailInputPage/EmailInputPage";
 import EmailCheckPage from "./page/Join/EmailCheckPage/EmailCheckPage";
--- frontend/page/Join/Join.jsx
+++
@@ -1,7 +0,0 @@
-import EmailInputPage from "./EmailInputPage/EmailInputPage";
-
-const Join = () => {
-  return <EmailInputPage />;
-};
-
-export default Join;
